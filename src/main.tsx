57bf3ff2be380bcfacb41943d1b1cf0ac918c0d7
¨sergey¨ <bboyruso@hotmail.com>
2023-05-30 16:19:02 +0200

    Add routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import GlobalStyle from "./GlobalStyle/GlobalStyle/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./GlobalStyle/theme/theme";
+import appRouter from "./routers/appRouter/appRouter";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <App />
+        <RouterProvider router={appRouter} />
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
