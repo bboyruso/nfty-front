cd4b9c806455b2d0ff87dc812b7c1adea6b81220
sergey <bboyruso@hotmail.com>
2023-05-31 11:15:57 +0200

    Refactor tests with RenderWithProviders

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,26 +1,12 @@
-import { render } from "@testing-library/react";
 import Button from "./Button";
-import { ThemeProvider } from "styled-components";
-import theme from "../../GlobalStyle/theme/theme";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a Button Component", () => {
   describe("When it receives a text content `Create`", () => {
     test("Then it should render with a text", () => {
       const expectedText = "Create";
-      const routes = [
-        {
-          path: "/",
-          element: (
-            <ThemeProvider theme={theme}>
-              <Button text={expectedText} />
-            </ThemeProvider>
-          ),
-        },
-      ];
 
-      const router = createMemoryRouter(routes);
-      render(<RouterProvider router={router} />);
+      renderWithProviders(<Button />);
 
       expect(expectedText).toBeInTheDocument;
     });
