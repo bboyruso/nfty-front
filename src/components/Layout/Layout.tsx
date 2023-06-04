3995a9248758e4b5e423f4e51c5bf8fc574375b1
bboyruso <bboyruso@hotmail.com>
2023-06-04 06:01:34 -0700

    Add Modal tests

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import MainPage from "../../pages/MainPage/MainPage";
-import Modal from "../Modal/Modal";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Modal isError={false} text="NFT not found"></Modal>
       <MainPage />
       <Outlet />
     </>
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,18 +3,18 @@ import { ReactSVG } from "react-svg";
 
 export interface ModalProps {
   text: string;
-  isError: boolean;
+  isError?: boolean;
 }
 
 const Modal = ({ text, isError }: ModalProps): React.ReactElement => {
   return (
     <ModalStyled className={isError ? "error" : ""}>
       <span>{text}</span>
-      {isError ? (
-        <ReactSVG src="src\assets\icons\error.svg"></ReactSVG>
-      ) : (
-        <ReactSVG src="src\assets\icons\done.svg"></ReactSVG>
-      )}
+      <ReactSVG
+        src={
+          isError ? "src/assets/icons/error.svg" : "src/assets/icons/done.svg"
+        }
+      />
     </ModalStyled>
   );
 };
