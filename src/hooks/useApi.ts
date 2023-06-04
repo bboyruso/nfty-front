0670d649fda20c153848330f5d8b3d14de0055f3
bboyruso <bboyruso@hotmail.com>
2023-06-04 11:47:14 -0700

    Add Error handling in api hook

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,22 +1,32 @@
 import { useCallback } from "react";
 import { NftStructure } from "../types";
 import axios from "axios";
-import { useAppDispatch } from "../store";
+import { useAppDispatch, useAppSelector } from "../store";
 import { startLoading } from "../store/loading/loadingSlice";
 import { stopLoading } from "../store/loading/loadingSlice";
+import { setError, clearError } from "../store/error/errorSlice";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const useApi = () => {
   const dispatch = useAppDispatch();
+  const errorMessage = useAppSelector((state) => state.error.message);
 
-  const getNfts = useCallback(async (): Promise<NftStructure[]> => {
-    dispatch(startLoading());
-    const { data: nfts } = await axios.get<NftStructure[]>(`${apiUrl}nfts`);
-    dispatch(stopLoading());
-    return nfts;
+  const getNfts = useCallback(async (): Promise<NftStructure[] | undefined> => {
+    try {
+      dispatch(clearError());
+      dispatch(startLoading());
+      const { data: nfts } = await axios.get<NftStructure[]>(`${apiUrl}nfts`);
+      dispatch(stopLoading());
+      return nfts;
+    } catch (error) {
+      dispatch(setError("Error getting NFTs"));
+      dispatch(stopLoading());
+      return undefined;
+    }
   }, [dispatch]);
-  return { getNfts };
+
+  return { getNfts, errorMessage };
 };
 
 export default useApi;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,24 +4,27 @@ import useApi from "../../hooks/useApi";
 import { loadNftsActionCreator } from "../../store/nfts/nftsSlice";
 import NftsList from "../../components/NftsList/NftsList";
 import Loading from "../../components/Loading/Loading";
+import Modal from "../../components/Modal/Modal";
 
 const MainPage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
-  const { getNfts } = useApi();
+  const { getNfts, errorMessage } = useApi();
   const { nfts } = useAppSelector((state) => state.nftsStore);
   const isLoading = useAppSelector((state) => state.loaderStore);
 
   useEffect(() => {
     (async () => {
       const nfts = await getNfts();
-      dispatch(loadNftsActionCreator(nfts));
+
+      if (nfts) dispatch(loadNftsActionCreator(nfts));
     })();
   }, [dispatch, getNfts]);
 
   return (
     <main aria-label="main page">
       {isLoading && <Loading />}
-      <NftsList nfts={nfts}></NftsList>
+      {errorMessage && <Modal text={errorMessage} isError />}
+      <NftsList nfts={nfts} />
     </main>
   );
 };
